a6badd4211714500294764c26a086f26c6ca75f0
foofangjee <foofangjee@gmail.com>
2021-01-14 01:14:56 +0800

    ball half done

--- Assignment.cpp
+++ Assignment.cpp
@@ -2,52 +2,53 @@
 #include <SFML/Audio.hpp>
 #include <Box2D/Box2D.h>
 #include <iostream>
+#include "Ball.h"
 
 int main(){
     //window creation
     int windowSizeX = 800;
     int windowSizeY = 600;
-    sf::RenderWindow window(sf::VideoMode(windowSizeX, windowSizeY), "Game Title here");
+    sf::RenderWindow window(sf::VideoMode(windowSizeX, windowSizeY), "Brick Breaker");
 
     // Set vSync to true (and as a result, we have a cap of 60FPS) - limit the framerate to a good value
 	window.setVerticalSyncEnabled(true);
 
-    //Load music to play 
-    sf::Music music;
-    if(!music.openFromFile("filename here"))
-        return EXIT_FAILURE;
-    music.play();
+    // //Load music to play 
+    // sf::Music music;
+    // if(!music.openFromFile("filename here"))
+    //     return EXIT_FAILURE;
+    // music.play();
 
-    //create a graphical text to display
-    sf::Font font;
-    if(!font.openFromFile("filename here"))
-        return EXIT_FAILURE;
-    sf::Text text("text here", font, fontsizehere);
+    // //create a graphical text to display
+    // sf::Font font;
+    // if(!font.openFromFile("filename here"))
+    //     return EXIT_FAILURE;
+    // sf::Text text("text here", font, fontsizehere);
 
     while(window.isOpen()){
         //process events
         sf::Event event;
 
         while(window.pollEvent(event)){
-            //Close window: exit
-            if(event.type == sf::Event::Closed)
-                window.close();
+            // //Close window: exit
+            // if(event.type == sf::Event::Closed)
+            //     window.close();
         }
 
-        //activate the window for OpenGL rendering
-        window.setActive();
+        // //activate the window for OpenGL rendering
+        // window.setActive();
 
-        //clear screen
-        window.clear();
+        // //clear screen
+        // window.clear();
 
-        //draw sprite
-        window.draw();
+        // //draw sprite
+        // window.draw();
 
-        //draw text
-        window.draw();
+        // //draw text
+        // window.draw();
 
-        //Update the window
-        window.display();
+        // //Update the window
+        // window.display();
     }
 
     return EXIT_SUCCESS;
